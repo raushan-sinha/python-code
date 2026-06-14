1eb44423e1182da660e025364e1464eac5ebde88
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-14 21:07:39 +0530

    Code commented

--- basics/index.py
+++ basics/index.py
@@ -1,26 +1,26 @@
-# Variables -
-user_name = "Anthropic"
-user_id = 12
-print("User name:", user_name)
-print("User id:", user_id)
+# # Variables -
+# user_name = "Anthropic"
+# user_id = 12
+# print("User name:", user_name)
+# print("User id:", user_id)
 
-# Data Types -
-name = "Claude AI"
-num = 23
-price = 23.44
-is_logged_in = True
-print(type(name))
-print(type(num))
-print(type(price))
-print(type(is_logged_in))
+# # Data Types -
+# name = "Claude AI"
+# num = 23
+# price = 23.44
+# is_logged_in = True
+# print(type(name))
+# print(type(num))
+# print(type(price))
+# print(type(is_logged_in))
 
-# Lists -
-skills = ["React", "JS", "TS", "Python"]
-print("Skills:", skills)
-print(type(skills))
+# # Lists -
+# skills = ["React", "JS", "TS", "Python"]
+# print("Skills:", skills)
+# print(type(skills))
 
-# Dictionary (dict) -
-details = {"name": "Raushan Sinha", "id": 34}
-print("Details:", details)
-print(type(details))
-print(details['name'])
+# # Dictionary (dict) -
+# details = {"name": "Raushan Sinha", "id": 34}
+# print("Details:", details)
+# print(type(details))
+# print(details['name'])
