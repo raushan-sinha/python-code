db740628b070da5238cea69bd0ff48e9ca0eeb45
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-14 20:48:12 +0530

    Added 'Data Types' code

--- basics/index.py
+++ basics/index.py
@@ -1,5 +1,15 @@
 # Variables -
-user_name = 'Anthropic'
+user_name = "Anthropic"
 user_id = 12
-print('User name:', user_name)
-print('User id:' , user_id)
+print("User name:", user_name)
+print("User id:", user_id)
+
+# Data Types -
+name = "Claude AI"
+num = 23
+price = 23.44
+is_logged_in = True
+print(type(name))
+print(type(num))
+print(type(price))
+print(type(is_logged_in))
