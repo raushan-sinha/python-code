8f31118ed2e24f2ed1943ddbc29be6ca6e681d7b
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-19 21:03:11 +0530

    Created a code for - Personal Profile Generator

---
+++ practice-code/basics.py
@@ -0,0 +1,8 @@
+# # Personal Profile Generator -
+# name = str(input('Enter your name: '))
+# age = int(input('Enter your age: '))
+# gender = str(input('Enter your gender: '))
+# city = str(input('Enter your city: '))
+
+# print(f'\n ------Profile------ \n\ * Name: {name} / \n\ * Age: {age} / \n\ * Gender: {gender} / \n\ * City: {city} / \n-------------')
+
