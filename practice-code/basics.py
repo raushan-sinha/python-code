c9b62d4c792050262990139d576236b858fd836c
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-06-27 23:55:25 +0530

    Code commented

--- practice-code/basics.py
+++ practice-code/basics.py
@@ -67,6 +67,7 @@
 #     print('Email is invalid')   
 
 
-# Print numbers from 1 to 100 -
-for i in range (100):
-    print(i)
+# # Print numbers from 1 to 100 -
+# for i in range (100):
+#     print(i)
+
